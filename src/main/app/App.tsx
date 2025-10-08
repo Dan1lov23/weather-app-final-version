53034541daeab98ce4ef0873a0230e093953cbb5
kitik <svy1dan@gmail.com>
2025-10-08 17:00:48 +0300

    app

---
+++ src/main/app/App.tsx
@@ -0,0 +1,11 @@
+import Home from "../../page component/Home/page/Home.tsx"
+
+export default function App() {
+    return (
+        <>
+            <div className="app">
+                <Home />
+            </div>
+        </>
+    )
+}
